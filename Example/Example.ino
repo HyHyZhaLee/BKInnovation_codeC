6c3a37c26aa859608bcc04212e5001f179a42e4f
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2023-10-24 07:31:29 +0700

    Commit test5

--- Example/Example.ino
+++ Example/Example.ino
@@ -28,11 +28,6 @@ float water_EC = 0, water_PH = 0, water_ORP = 0, water_temp = 0;
 float air_temperature, air_humidity, air_illuminance = 0, air_CO2 = 0;
 float soil_PH = 1, soil_temperature = 2, soil_humidity = 3, soil_N = 4, soil_P = 5, soil_K= 6, soil_EC =7;
 
-// String floatToString(float value) {
-//   char buffer[20];  // Đủ lớn để chứa chuỗi
-//   sprintf(buffer, "%.2f", value);
-//   return String(buffer);
-// }
 
 String createWaterStationJSON(float EC, float PH, float ORP, float TEMP) {
   DynamicJsonDocument doc(1024);
@@ -70,6 +65,10 @@ String createWaterStationJSON(float EC, float PH, float ORP, float TEMP) {
 
   String jsonString;
   serializeJson(doc, jsonString);
+  Serial.println("Data to pub:");
+  serializeJsonPretty(doc, Serial);
+  doc.clear();
+  Serial.println();
   return jsonString;
 }
 
@@ -112,7 +111,67 @@ String createAirStationJSON(float temp, float humi, float illuminance, float co2
   Serial.println("Data to pub:");
   serializeJsonPretty(doc, Serial);
   doc.clear();
-  Serial.println()
+  Serial.println();
+  return jsonString;
+}
+String createSoilStationJSON(float temp, float humi, float ph, float ec, float nito, float photpho, float kali) {
+  DynamicJsonDocument doc(1024);
+
+  doc["station_id"] = "SOIL_0001";
+  doc["station_name"] = "SOIL 0001";
+  doc["gps_longitude"] = 106.89;
+  doc["gps_latitude"] = 10.5;
+
+  JsonArray sensors = doc.createNestedArray("sensors");
+
+  JsonObject temp_sensor = sensors.createNestedObject();
+  temp_sensor["sensor_id"] = "temp_0001";
+  temp_sensor["sensor_name"] = "Nhiệt Độ";
+  temp_sensor["sensor_value"] = floatToString(temp);
+  temp_sensor["sensor_unit"] = "°C";
+
+  JsonObject humi_sensor = sensors.createNestedObject();
+  humi_sensor["sensor_id"] = "humi_0001";
+  humi_sensor["sensor_name"] = "Độ Ẩm";
+  humi_sensor["sensor_value"] = floatToString(humi);
+  humi_sensor["sensor_unit"] = "%";
+
+  JsonObject ph_sensor = sensors.createNestedObject();
+  ph_sensor["sensor_id"] = "ph_0001";
+  ph_sensor["sensor_name"] = "PH";
+  ph_sensor["sensor_value"] = floatToString(ph);
+  ph_sensor["sensor_unit"] = "";
+
+  JsonObject ec_sensor = sensors.createNestedObject();
+  ec_sensor["sensor_id"] = "EC_0001";
+  ec_sensor["sensor_name"] = "EC";
+  ec_sensor["sensor_value"] = floatToString(ec);
+  ec_sensor["sensor_unit"] = "ms/cm";
+
+  JsonObject nito_sensor = sensors.createNestedObject();
+  nito_sensor["sensor_id"] = "Nito_0001";
+  nito_sensor["sensor_name"] = "N";
+  nito_sensor["sensor_value"] = floatToString(nito);
+  nito_sensor["sensor_unit"] = "ms/cm";
+
+  JsonObject photpho_sensor = sensors.createNestedObject();
+  photpho_sensor["sensor_id"] = "Photpho_0001";
+  photpho_sensor["sensor_name"] = "P";
+  photpho_sensor["sensor_value"] = floatToString(photpho);
+  photpho_sensor["sensor_unit"] = "ms/cm";
+
+  JsonObject kali_sensor = sensors.createNestedObject();
+  kali_sensor["sensor_id"] = "Kali_0001";
+  kali_sensor["sensor_name"] = "K";
+  kali_sensor["sensor_value"] = floatToString(kali);
+  kali_sensor["sensor_unit"] = "ms/cm";
+
+  String jsonString;
+  serializeJson(doc, jsonString);
+  Serial.println("Data to pub:");
+  serializeJsonPretty(doc, Serial);
+  doc.clear();
+  Serial.println();
   return jsonString;
 }
 
@@ -152,96 +211,95 @@ void loop() {
   }
   delay(5000);
 
-  // Serial.println("Writing to soil PH with data...");
-  // Serial2.write(data485.getDataSOIL_PH(), 8);
-  // delay(1000);
-  // if (Serial2.available()) {    // If the serial port receives a message. 
-  //   uint8_t receivedData[7];
-  //   Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the message.
-  //   for (int i = 0; i <7 ; i++) {
-  //     Serial.print("0x");
-  //     Serial.print(receivedData[i], HEX);
-  //     Serial.print(", ");
-  //   }
-  //   Serial.println();
-  //   soil_PH = (int)receivedData[3]*256 + (int)receivedData[4];
-  //   soil_PH/=100.0;
-  //   Serial.print("Soil PH = ");
-  //   Serial.println(soil_PH);
-  // }
-  // delay(5000);
+  Serial.println("Writing to soil PH with data...");
+  Serial2.write(data485.getDataSOIL_PH(), 8);
+  delay(1000);
+  if (Serial2.available()) {    // If the serial port receives a message. 
+    uint8_t receivedData[7];
+    Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the message.
+    for (int i = 0; i <7 ; i++) {
+      Serial.print("0x");
+      Serial.print(receivedData[i], HEX);
+      Serial.print(", ");
+    }
+    Serial.println();
+    soil_PH = (int)receivedData[3]*256 + (int)receivedData[4];
+    soil_PH/=100.0;
+    Serial.print("Soil PH = ");
+    Serial.println(soil_PH);
+  }
+  delay(5000);
 
-  // myMQTT.checkConnect();
-  // Serial.println("Writing to soil temperature and humidity with data...");
-  // Serial2.write(data485.getDataSOIL_TEMPERATURE_HUMIDITY(), 8);
-  // delay(1000);
-  // if (Serial2.available()) {    // If the serial port receives a message. 
-  //   uint8_t receivedData[9];
-  //   Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the message.
-  //   for (int i = 0; i <9 ; i++) {
-  //     Serial.print("0x");
-  //     Serial.print(receivedData[i], HEX);
-  //     Serial.print(", ");
-  //   }
-  //   Serial.println();
-  //   soil_humidity = (int)receivedData[3]*256 + (int)receivedData[4];
-  //   soil_temperature = (int)receivedData[5]*256 + (int)receivedData[6];
-  //   soil_humidity/=10.0;
-  //   soil_temperature/=10.0;
-  //   Serial.print("Soil humidity: "); 
-  //   Serial.println(soil_humidity);
-  //   Serial.print("Soil temperature: ");
-  //   Serial.println(soil_temperature);
-  // }
-  // delay(5000);
+  myMQTT.checkConnect();
+  Serial.println("Writing to soil temperature and humidity with data...");
+  Serial2.write(data485.getDataSOIL_TEMPERATURE_HUMIDITY(), 8);
+  delay(1000);
+  if (Serial2.available()) {    // If the serial port receives a message. 
+    uint8_t receivedData[9];
+    Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the message.
+    for (int i = 0; i <9 ; i++) {
+      Serial.print("0x");
+      Serial.print(receivedData[i], HEX);
+      Serial.print(", ");
+    }
+    Serial.println();
+    soil_humidity = (int)receivedData[3]*256 + (int)receivedData[4];
+    soil_temperature = (int)receivedData[5]*256 + (int)receivedData[6];
+    soil_humidity/=10.0;
+    soil_temperature/=10.0;
+    Serial.print("Soil humidity: "); 
+    Serial.println(soil_humidity);
+    Serial.print("Soil temperature: ");
+    Serial.println(soil_temperature);
+  }
+  delay(5000);
 
-  // Serial.println("Writing to soil NPK with data...");
-  // Serial2.write(data485.getDataSOIL_NPK(), 8);
-  // delay(1000);
-  // if (Serial2.available()) {    // If the serial port receives a message. 
-  //   uint8_t receivedData[11];
-  //   Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the message.
-  //   for (int i = 0; i <11 ; i++) {
-  //     Serial.print("0x");
-  //     Serial.print(receivedData[i], HEX);
-  //     Serial.print(", ");
-  //   }
-  //   Serial.println();
-  //   soil_N = (int)receivedData[3]*256 + (int)receivedData[4];
-  //   soil_P = (int)receivedData[5]*256 + (int)receivedData[6];
-  //   soil_K = (int)receivedData[7]*256 + (int)receivedData[8];
-  //   Serial.print("Soil Nito: "); 
-  //   Serial.println(soil_N);
-  //   Serial.print("Soil Photpho: ");
-  //   Serial.println(soil_P);
-  //   Serial.print("Soil Kali: ");
-  //   Serial.println(soil_K);
-  // }
-  // delay(5000);
+  Serial.println("Writing to soil NPK with data...");
+  Serial2.write(data485.getDataSOIL_NPK(), 8);
+  delay(1000);
+  if (Serial2.available()) {    // If the serial port receives a message. 
+    uint8_t receivedData[11];
+    Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the message.
+    for (int i = 0; i <11 ; i++) {
+      Serial.print("0x");
+      Serial.print(receivedData[i], HEX);
+      Serial.print(", ");
+    }
+    Serial.println();
+    soil_N = (int)receivedData[3]*256 + (int)receivedData[4];
+    soil_P = (int)receivedData[5]*256 + (int)receivedData[6];
+    soil_K = (int)receivedData[7]*256 + (int)receivedData[8];
+    Serial.print("Soil Nito: "); 
+    Serial.println(soil_N);
+    Serial.print("Soil Photpho: ");
+    Serial.println(soil_P);
+    Serial.print("Soil Kali: ");
+    Serial.println(soil_K);
+  }
+  delay(5000);
   
 
-  // Serial.println("Writing to soil conductivity with data...");
-  // Serial2.write(data485.getDataSOIL_CONDUCTIVITY(), 8);
-  // delay(1000);
-  // if (Serial2.available()) {    // If the serial port receives a message. 
-  //   uint8_t receivedData[7];
-  //   Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the message.
-  //   for (int i = 0; i <7 ; i++) {
-  //     Serial.print("0x");
-  //     Serial.print(receivedData[i], HEX);
-  //     Serial.print(", ");
-  //   }
-  //   Serial.println();
-  //   soil_EC = (int)receivedData[3]*256 + (int)receivedData[4];
-  //   Serial.print("Soil EC: "); 
-  //   Serial.println(soil_EC);
-  // }
+  Serial.println("Writing to soil conductivity with data...");
+  Serial2.write(data485.getDataSOIL_CONDUCTIVITY(), 8);
+  delay(1000);
+  if (Serial2.available()) {    // If the serial port receives a message. 
+    uint8_t receivedData[7];
+    Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the message.
+    for (int i = 0; i <7 ; i++) {
+      Serial.print("0x");
+      Serial.print(receivedData[i], HEX);
+      Serial.print(", ");
+    }
+    Serial.println();
+    soil_EC = (int)receivedData[3]*256 + (int)receivedData[4];
+    Serial.print("Soil EC: "); 
+    Serial.println(soil_EC);
+  }
   // data.setValueSOIL_STATION(soil_temperature,soil_humidity,soil_PH,soil_EC,soil_N,soil_P,soil_K);
-  // myMQTT.publish("/innovation/soilmonitoring/", data.getData().c_str()); // Publish to feed
-  // Serial.println("Data to pub:");
-  // data.print();
-  // Serial.println();
-  // delay(5000);
+  String data_to_pub = createSoilStationJSON(soil_temperature,soil_humidity,soil_PH,soil_EC,soil_N,soil_P,soil_K);
+  myMQTT.publish("/innovation/soilmonitoring/", data_to_pub); // Publish to feed
+  Serial.println();
+  delay(5000);
   M5.update();
 }
 
